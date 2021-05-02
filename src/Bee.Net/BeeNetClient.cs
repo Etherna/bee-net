158d8ef832f6d8725b2d4e66d186aba6af8b5f9b
Mirko Da Corte <mirko@etherna.io>
2021-05-02 19:14:58 +0200

    Implemented BeeNetClient

--- src/Bee.Net/BeeNetClient.cs
+++ src/Bee.Net/BeeNetClient.cs
@@ -1,27 +1,51 @@
 ﻿using Etherna.BeeNet.Clients.DebugApi;
 using Etherna.BeeNet.Clients.GatewayApi;
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Diagnostics.CodeAnalysis;
+using System.Net.Http;
 
 namespace Etherna.BeeNet
 {
-    public class BeeNetClient : IBeeNetClient
+    public class BeeNetClient : IBeeNetClient, IDisposable
     {
         // Fields.
-        private readonly int debugApiPort;
-        private readonly int gatewayApiPort;
+        private readonly HttpClient httpClient;
+        private bool disposed;
 
         // Constructors.
+        [SuppressMessage("Design", "CA1054:URI-like parameters should not be strings",
+            Justification = "A string is required by Nswag generated client")]
         public BeeNetClient(
             string baseUrl = "http://localhost",
             int gatewayApiPort = 1633,
             int debugApiPort = 1635)
         {
-            this.debugApiPort = debugApiPort;
-            this.gatewayApiPort = gatewayApiPort;
+            httpClient = new HttpClient();
 
             // Generate api clients.
+            BeeDebugClient = new BeeDebugClient(httpClient)
+            { BaseUrl = string.Concat(baseUrl, ':', debugApiPort) };
+
+            BeeGatewayClient = new BeeGatewayClient(httpClient)
+            { BaseUrl = string.Concat(baseUrl, ':', gatewayApiPort) };
+        }
+
+        // Dispose.
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+
+            // Dispose managed resources.
+            if (disposing)
+                httpClient.Dispose();
+
+            disposed = true;
         }
 
         // Properties.
