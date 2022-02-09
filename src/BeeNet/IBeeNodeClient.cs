3511b1bcda45d676540187917a11f62e9528998d
Mirko Da Corte <mirko@etherna.io>
2022-02-09 17:58:35 +0100

    Add license headers

--- src/BeeNet/IBeeNodeClient.cs
+++ src/BeeNet/IBeeNodeClient.cs
@@ -12,7 +12,6 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-
 using Etherna.BeeNet.Clients;
 using Etherna.BeeNet.Clients.DebugApi;
 using Etherna.BeeNet.Clients.GatewayApi;
