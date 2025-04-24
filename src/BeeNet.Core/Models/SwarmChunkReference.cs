3467712ae370effa23e167c0bbc663caaeb8d54f
Mirko Da Corte <mirko@etherna.io>
2025-04-24 21:08:44 +0200

    Add static builder method to resolve SwarmChunkReference from string

--- src/BeeNet.Core/Models/SwarmChunkReference.cs
+++ src/BeeNet.Core/Models/SwarmChunkReference.cs
@@ -1,3 +1,7 @@
+using Etherna.BeeNet.Manifest;
+using Etherna.BeeNet.Stores;
+using System.Threading.Tasks;
+
 namespace Etherna.BeeNet.Models
 {
     public class SwarmChunkReference(
@@ -5,6 +9,19 @@ namespace Etherna.BeeNet.Models
         XorEncryptKey? encryptionKey,
         bool useRecursiveEncryption)
     {
+        // Static builders.
+        public static async Task<SwarmChunkReference> ResolveFromStringAsync(
+            string hashOrAddress,
+            IReadOnlyChunkStore chunkStore)
+        {
+            if (SwarmHash.IsValidHash(hashOrAddress))
+                return new SwarmChunkReference(SwarmHash.FromString(hashOrAddress), null, false);
+            return (await SwarmAddress.FromString(hashOrAddress).ResolveToResourceInfoAsync(
+                    chunkStore, ManifestPathResolver.IdentityResolver).ConfigureAwait(false))
+                .Result.ChunkReference;
+        }
+
+        // Properties.
         public XorEncryptKey? EncryptionKey { get; } = encryptionKey;
         public SwarmHash Hash { get; } = hash;
         public bool UseRecursiveEncryption { get; } = useRecursiveEncryption;
