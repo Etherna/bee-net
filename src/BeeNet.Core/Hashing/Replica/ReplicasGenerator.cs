b97fe54a0d7def5aefd897a698c1a46a32cce9e9
Mirko Da Corte <mirko@etherna.io>
2025-10-17 18:26:47 +0200

    add ReplicasGenerator

---
+++ src/BeeNet.Core/Hashing/Replica/ReplicasGenerator.cs
@@ -0,0 +1,114 @@
+// Copyright 2021-present Etherna SA
+// This file is part of Bee.Net.
+// 
+// Bee.Net is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Lesser General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+// 
+// Bee.Net is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License along with Bee.Net.
+// If not, see <https://www.gnu.org/licenses/>.
+
+using Etherna.BeeNet.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Etherna.BeeNet.Hashing.Replica
+{
+    public static class ReplicasGenerator
+    {
+        /// <summary>
+        /// Generate an array of replica Ids from a hash and a redundancy level.
+        /// Order of replicas is so that addresses are always maximally dispersed in successive sets of addresses.
+        /// </summary>
+        /// <param name="hash">Input hash</param>
+        /// <param name="redundancyLevel">Input redundancy level</param>
+        /// <param name="hasher">Hasher</param>
+        /// <returns>Replica Ids</returns>
+        public static SwarmReplicaId[] GenerateReplicaIds(
+            SwarmHash hash,
+            RedundancyLevel redundancyLevel,
+            Hasher hasher)
+        {
+            ArgumentNullException.ThrowIfNull(hasher, nameof(hasher));
+
+            if (redundancyLevel == RedundancyLevel.None)
+                return [];
+            
+            // For the five levels of redundancy, the actual numbers of replicas needed to keep
+            // the error rate below 1/10^6 are [0, 2, 4, 5, 19], but we use an approximation to powers of 2.
+            var targetReplicas = 1 << (int)redundancyLevel;
+            var foundNeighborhoodsByDepth = new bool[(int)redundancyLevel][];
+            for (var i = 1; i <= (int)redundancyLevel; i++)
+                foundNeighborhoodsByDepth[i - 1] = new bool[1 << i];
+            var replicaIds = new List<SwarmReplicaId>();
+            
+            // Build queue and cursor.
+            var queue = new SwarmReplicaId?[targetReplicas];
+            var queueCursorsByDepth = new int[(int)redundancyLevel];
+            for (int i = 1; i < (int)redundancyLevel; i++)
+                queueCursorsByDepth[i] = 1 << i;
+            
+            // Search replica ids.
+            for (byte i = 0; i < 255 && replicaIds.Count < targetReplicas; i++)
+            {
+                // Generate Soc Id and Hash.
+                var socIdArray = hash.ToByteArray();
+                socIdArray[0] = i;
+                var socHash = SwarmSoc.BuildHash(socIdArray, SwarmSoc.ReplicasOwner, hasher);
+
+                // Try to add new replica to queue, and drain it.
+                var isValid = TryAddAtDepth(
+                    new SwarmReplicaId
+                    {
+                        Hash = socHash,
+                        SocId = socIdArray
+                    },
+                    (int)redundancyLevel,
+                    foundNeighborhoodsByDepth,
+                    queueCursorsByDepth,
+                    queue);
+
+                if (!isValid) continue;
+                
+                for (var j = replicaIds.Count; j < queue.Length; j++)
+                {
+                    if (queue[j] == null) break;
+                    replicaIds.Add(queue[j]!);
+                }
+            }
+            
+            return replicaIds.ToArray();
+        }
+
+        private static bool TryAddAtDepth(
+            SwarmReplicaId replicaId,
+            int depth,
+            bool[][] foundNeighborhoodsByDepth,
+            int[] queueCursorsByDepth,
+            SwarmReplicaId?[] queue)
+        {
+            if (depth == 0)
+                return false;
+
+            // Find neighborhood and verify collisions.
+            var neighborhood = replicaId.Hash.ToReadOnlyMemory().Span[0] >> (8 - (byte)depth);
+            if (foundNeighborhoodsByDepth[depth - 1][neighborhood])
+                return false;
+            foundNeighborhoodsByDepth[depth - 1][neighborhood] = true;
+            
+            // Try to add at lower depth.
+            if (TryAddAtDepth(replicaId, depth - 1, foundNeighborhoodsByDepth, queueCursorsByDepth, queue))
+                return true;
+            
+            // If recursion didn't insert at lower depth, insert here.
+            queue[queueCursorsByDepth[depth - 1]] = replicaId;
+            queueCursorsByDepth[depth - 1]++;
+
+            return true;
+        }
+    }
+}
